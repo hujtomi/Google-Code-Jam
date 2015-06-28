941c2fa7af420460b3700fb60d87541fa1522f74
tomi <hujtomi@gmail.com>
2015-06-28 14:49:25 +0200

    my solution for this problem: https://code.google.com/codejam/contest/dashboard?c=2974486#s=p1

---
+++ CodeJam2014.CookieClickerAlpha/CodeJam2014.CookieClickerAlpha/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeJam2014.CookieClickerAlpha
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new Program();
+        }
+
+        public Program()
+        {
+            StringBuilder sb = new StringBuilder();
+            string[] lines = System.IO.File.ReadAllLines(@"C:\Users\Tamás\SkyDrive\Dokumentumok\Visual Studio 2013\CodeJam2014.CookieClickerAlpha\CodeJam2014.CookieClickerAlpha\input.in");
+
+            int cases = int.Parse(lines[0]);
+
+            for (int i = 0; i < cases; i++)
+            {
+                double cookiePerSec = 2;
+                
+                string[] values = lines[i + 1].Split(' ');
+
+                double C = double.Parse(values[0], CultureInfo.InvariantCulture);
+                double F = double.Parse(values[1], CultureInfo.InvariantCulture);
+                double X = double.Parse(values[2], CultureInfo.InvariantCulture);
+
+                double totalTime = X / cookiePerSec;
+                //double totalTimeWith1Farm = C / cookiePerSec + X / (cookiePerSec + F);
+                //double totalTimeWith2Farm = C / cookiePerSec + C / (cookiePerSec + F) + X / (cookiePerSec + 2 * F);
+
+                int f = 1;
+
+                double newTotalTime = 0;
+
+                do
+                {
+                    if (newTotalTime != 0 && newTotalTime < totalTime) {
+                        totalTime = newTotalTime;
+                    }
+
+                    newTotalTime = 0;
+
+                    for (int k = 0; k < f; k++)
+			        {
+			            newTotalTime += C / (k * F + cookiePerSec); 
+			        }
+
+                    newTotalTime += X / (cookiePerSec + f * F);
+                    f++;
+                }
+                while (newTotalTime < totalTime);
+
+                sb.AppendLine(string.Format("Case #{0}: {1}", i + 1, totalTime.ToString("F7").Replace(',', '.')));
+
+            }
+
+            //Console.WriteLine(sb.ToString());
+
+            using (System.IO.StreamWriter file = new System.IO.StreamWriter(@"C:\Users\Tamás\SkyDrive\Dokumentumok\Visual Studio 2013\CodeJam2014.CookieClickerAlpha\CodeJam2014.CookieClickerAlpha\\solution.out"))
+            {
+                file.Write(sb.ToString());
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -4,26 +4,26 @@ source codes for google code jams and topcpder SRMs
 ##Google code jam problems
 
 Problem A. The Repeater
-
 https://code.google.com/codejam/contest/2994486/dashboard#s=p0
 
 my solution:
-
 https://github.com/hujtomi/Google-Code-Jam/tree/master/CodeJam2014.TheRepeater
 
 Problem A. Magic Trick
-
 https://code.google.com/codejam/contest/2974486/dashboard
 
 my solution:
-
 https://github.com/hujtomi/Google-Code-Jam/tree/master/CodeJam2014.MagicTrick
 
+Problem B. Cookie Clicker Alpha
+https://code.google.com/codejam/contest/dashboard?c=2974486#s=p1
+
+my solution:
+https://github.com/hujtomi/Google-Code-Jam/tree/master/CodeJam2014.CookieClickerAlpha
 
 ##Topcoder SRMs
 
 http://topblogcoder.com/topcoder-c-srm-600-div-2/
 
 my solution:
-
 https://github.com/hujtomi/Google-Code-Jam/tree/master/theshuttles-java
