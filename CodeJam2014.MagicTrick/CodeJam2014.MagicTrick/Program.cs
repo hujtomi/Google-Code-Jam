f854945a911fa68ed40806014381605471fb3b91
tomi <hujtomi@gmail.com>
2015-06-28 14:44:43 +0200

    my solution for this problem: https://code.google.com/codejam/contest/2974486/dashboard

---
+++ CodeJam2014.MagicTrick/CodeJam2014.MagicTrick/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeJam2014.MagicTrick
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new Program();
+        }
+
+        public Program()
+        {
+            StringBuilder sb = new StringBuilder();
+            string[] lines = System.IO.File.ReadAllLines(@"..\..\first.in");
+
+            int cases = int.Parse(lines[0]);
+
+            for (int i = 0; i < cases; i++)
+            {
+                int firstAnsware = int.Parse( lines[1 + i * 10] );
+
+                string[] firstRow = lines[1 + i * 10 + firstAnsware].Split(' ');
+
+                int secoundAnsware = int.Parse(lines[1 + 5 + i * 10]);
+
+                string[] secoundRow = lines[1 + 5 + i * 10 + secoundAnsware].Split(' ');
+
+                int solutionCounter = 0;
+                string solution = string.Empty;
+                foreach (var first in firstRow)
+                {
+                    foreach (var secound in secoundRow)
+                    {
+                        if (first == secound) {
+                            solution = first;
+                            solutionCounter++;
+                        }
+                    }
+                }
+
+                if (solutionCounter == 0)
+                {
+                    sb.AppendLine(string.Format("Case #{0}: {1}", i + 1, "Volunteer cheated!"));
+                }
+                else if (solutionCounter > 1)
+                {
+                    sb.AppendLine(string.Format("Case #{0}: {1}", i + 1, "Bad magician!"));
+                }
+                else {
+                    sb.AppendLine(string.Format("Case #{0}: {1}", i + 1, solution));
+                }
+            }
+
+            using (System.IO.StreamWriter file = new System.IO.StreamWriter(@"..\..\solution.out"))
+            {
+                file.Write(sb.ToString());
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,14 +1,29 @@
 # Google-Code-Jam
 source codes for google code jams and topcpder SRMs
 
-Google code jam problem:
+##Google code jam problems
+
+Problem A. The Repeater
+
 https://code.google.com/codejam/contest/2994486/dashboard#s=p0
+
 my solution:
+
 https://github.com/hujtomi/Google-Code-Jam/tree/master/CodeJam2014.TheRepeater
 
+Problem A. Magic Trick
 
-Topcoder SRMs
+https://code.google.com/codejam/contest/2974486/dashboard
+
+my solution:
+
+https://github.com/hujtomi/Google-Code-Jam/tree/master/CodeJam2014.MagicTrick
+
+
+##Topcoder SRMs
 
 http://topblogcoder.com/topcoder-c-srm-600-div-2/
+
 my solution:
+
 https://github.com/hujtomi/Google-Code-Jam/tree/master/theshuttles-java
