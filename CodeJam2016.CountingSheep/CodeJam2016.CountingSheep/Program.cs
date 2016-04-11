cae337ef6a115ae522a0c9f35d8f965d9fafcda0
hujtomi <hujtomi@gmail.com>
2016-04-12 00:40:02 +0200

    code jam 2016 qualification round

---
+++ CodeJam2016.CountingSheep/CodeJam2016.CountingSheep/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeJam2016.CountingSheep
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new Program();
+        }
+
+        public Program()
+        {
+            StringBuilder result = new StringBuilder();
+            var lines = File.ReadAllLines("input.txt");
+
+            bool[] digits = new bool[10];
+
+            for (int i = 1; i < lines.Length; i++)
+            {
+                for (int j = 0; j < digits.Length; j++)
+                {
+                    digits[j] = false;
+                }
+
+                if (long.Parse(lines[i]) == 0)
+                {
+                    result.AppendLine($"Case #{i}: INSOMNIA");
+                    continue;
+                }
+                
+                int multiplier = 1;
+                do
+                {
+                    long number = long.Parse(lines[i]) * multiplier;
+                    multiplier++;
+
+                    char[] letters = number.ToString().ToCharArray();
+                    foreach (var letter in letters)
+                    {
+                        digits[int.Parse(letter.ToString())] = true;
+                    }
+
+                    if(digits.All(d => d))
+                    {
+                        result.AppendLine($"Case #{i}: {number}");
+                        break;
+                    }                    
+                } while (true);                
+            }
+
+            Console.WriteLine(result);
+            File.WriteAllText("output.txt", result.ToString());
+        }
+    }
+}
---
+++ CodeJam2016.RevengeOfThePancakes/CodeJam2016.RevengeOfThePancakes/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeJam2016.RevengeOfThePancakes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new Program();
+        }
+
+        public Program()
+        {
+            StringBuilder result = new StringBuilder();
+            var lines = File.ReadAllLines("input.txt");
+
+            for (int i = 1; i < lines.Length; i++)
+            {
+                int flipCounter = 0;
+                string line = lines[i].Trim();
+
+                char[] letters = line.ToCharArray();
+                bool[] pencakeStack = new bool[letters.Length];
+
+                for (int j = 0; j < letters.Length; j++)
+                {
+                    pencakeStack[j] = letters[j] == '+';
+                }
+
+                for (int j = 0; j < letters.Length; j++)
+                {
+                    if(!pencakeStack[letters.Length - 1 - j])
+                    {
+                        flipCounter++;
+
+                        for (int k = 0; k < letters.Length - 1 - j; k++)
+                        {
+                            pencakeStack[k] = !pencakeStack[k];
+                        }
+                    }
+                }
+
+                result.AppendLine($"Case #{i}: {flipCounter}");
+            }
+
+            Console.WriteLine(result);
+            File.WriteAllText("output.txt", result.ToString());
+        }
+    }
+}
