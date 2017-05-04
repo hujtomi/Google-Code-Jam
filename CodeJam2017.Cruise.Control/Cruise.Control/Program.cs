732df81213b5801fe2c5d5a1d279e82bc3b689fd
hujtomi <hujtomi@gmail.com>
2017-05-04 22:51:03 +0200

    solution to https://code.google.com/codejam/contest/8294486/dashboard

---
+++ CodeJam2017.Cruise.Control/Cruise.Control/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+using System.Text;
+
+namespace Cruise.Control
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] lines = File.ReadAllLines("input.txt");
+
+            NumberFormatInfo nfi = new NumberFormatInfo();
+            nfi.NumberDecimalSeparator = ".";
+
+            StringBuilder sb = new StringBuilder();
+            int caseCounter = 1;
+            for (int i = 1; i < lines.Length; i++)
+            {
+                double d = double.Parse(lines[i].Split(new char[] { ' ' })[0]);
+                double n = double.Parse(lines[i].Split(new char[] { ' ' })[1]);
+
+                double fullTimeRequired = 0;
+                for (int j = 0; j < n; j++)
+                {
+                    i++;
+                    double k = double.Parse(lines[i].Split(new char[] { ' ' })[0]);
+                    double s = double.Parse(lines[i].Split(new char[] { ' ' })[1]);
+                    
+                    double timeRequired = (d - k) / s;
+
+                    if (timeRequired > fullTimeRequired)
+                        fullTimeRequired = timeRequired;
+                }
+
+                sb.AppendLine($"Case #{caseCounter}: {Math.Round(d / fullTimeRequired, 6).ToString("F6", nfi)}");
+                caseCounter++;
+            }
+
+            File.WriteAllText("output.txt", sb.ToString());
+        }
+    }
+}
