4cfd2d4a861d3efb6273753f4df2c890ec063b38
hujtomi <hujtomi@gmail.com>
2017-05-04 23:15:47 +0200

    solutions to https://code.google.com/codejam/contest/3264486/dashboard

---
+++ CodeJam2017.Oversized.Pancake.Flipper/Oversized.Pancake.Flipper/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Linq;
+
+namespace Oversized.Pancake.Flipper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] lines = File.ReadAllLines("input.txt");
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 1; i < lines.Length; i++)
+            {
+                string patternString = lines[i].Split(new char[] { ' ' })[0];
+                int flipperSize = int.Parse(lines[i].Split(new char[] { ' ' })[1]);
+
+                bool[] pattern = new bool[patternString.Length];
+
+                var patternChars = patternString.ToCharArray();
+                for (int j = 0; j < patternString.Length; j++)
+                {
+                    pattern[j] = patternChars[j] == '+';
+                }
+
+                int flipCount = 0;
+                for (int j = 0; j <= pattern.Length - flipperSize; j++)
+                {
+                    if (!pattern[j])
+                    {
+                        flipCount++;
+                        for (int k = 0; k < flipperSize; k++)
+                        {
+                            pattern[j + k] = !pattern[j + k];
+                        }
+                    }
+                }
+
+                if(pattern.Any(p => p == false))
+                {
+                    sb.AppendLine($"Case #{i}: IMPOSSIBLE");
+                } else
+                {
+                    sb.AppendLine($"Case #{i}: {flipCount}");
+                }
+            }
+
+            File.WriteAllText("output.txt", sb.ToString());
+        }
+    }
+}
---
+++ CodeJam2017.Tidy.Numbers/Tidy.Numbers/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.IO;
+using System.Numerics;
+using System.Text;
+
+namespace Tidy.Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = File.ReadAllLines("input.txt");
+            StringBuilder output = new StringBuilder();
+
+            for (int i = 1; i < input.Length; i++)
+            {
+                BigInteger value = BigInteger.Parse(input[i]);
+
+                if(value.ToString().Length == 1)
+                {
+                    output.AppendLine($"Case #{i}: {value}");
+                    continue;
+                }
+
+                var digits = value.ToString().ToCharArray();
+                for (int j = 1; j < digits.Length; j++)
+                {
+                    if(digits[j-1] > digits[j])
+                    {                        
+                        var diff = BigInteger.Pow(new BigInteger(10), digits.Length - 1 - j);
+                        value = BigInteger.Add(value, BigInteger.Multiply(diff, BigInteger.MinusOne));
+
+                        if (digits[j] == '0')
+                        {
+                            digits = value.ToString().ToCharArray();
+                            for (int k = j; k < digits.Length; k++)
+                            {
+                                digits[k] = '9';
+                            }
+                            value = BigInteger.Parse(new string(digits));
+                        }
+                        else
+                            digits = value.ToString().ToCharArray();
+
+                        j = 0;
+                    }
+                }
+
+                output.AppendLine($"Case #{i}: {value}");
+            }
+
+            File.WriteAllText("output.txt", output.ToString());
+        }
+    }
+}
